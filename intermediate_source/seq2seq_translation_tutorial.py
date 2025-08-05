398f3f70f1bc2f1baa377ef1c9722548c6c71c89
P. Tan <zxtanph@gmail.com>
2025-08-05 18:40:39 -0400

    Fixed some plot warnings and make plot to show

--- intermediate_source/seq2seq_translation_tutorial.py
+++ intermediate_source/seq2seq_translation_tutorial.py
@@ -707,7 +707,7 @@ def train(train_dataloader, encoder, decoder, n_epochs, learning_rate=0.001,
 #
 
 import matplotlib.pyplot as plt
-plt.switch_backend('agg')
+#plt.switch_backend('agg')
 import matplotlib.ticker as ticker
 import numpy as np
 
@@ -824,9 +824,18 @@ def showAttention(input_sentence, output_words, attentions):
     fig.colorbar(cax)
 
     # Set up axes
-    ax.set_xticklabels([''] + input_sentence.split(' ') +
-                       ['<EOS>'], rotation=90)
-    ax.set_yticklabels([''] + output_words)
+    # ax.set_xticklabels([''] + input_sentence.split(' ') +
+    #                    ['<EOS>'], rotation=90)
+    # ax.set_yticklabels([''] + output_words)
+
+    # Set up axes
+    x_labels = [''] + input_sentence.split(' ') + ['<EOS>']
+    y_labels = [''] + output_words
+
+    ax.set_xticks(range(len(x_labels)))
+    ax.set_yticks(range(len(y_labels)))
+    ax.set_xticklabels(x_labels, rotation=90)
+    ax.set_yticklabels(y_labels)
 
     # Show label at every tick
     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
